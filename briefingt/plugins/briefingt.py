4c5e2316d8951fa5dc47ccb0978728e1539ae2a4
thriuin <ross.thompson@tbs-sct.gc.ca>
2024-01-04 16:50:22 -0500

    OPEN 2941 and OPEN 2873: Add support for Nil Reports to QP Notes and Briefing Titles

--- briefingt/plugins/briefingt.py
+++ briefingt/plugins/briefingt.py
@@ -28,7 +28,9 @@ def pre_export_solr_query(solr_query: dict, request: HttpRequest, search: Search
 
 
 def post_export_solr_query(solr_response: SolrResponse, solr_query: dict, request: HttpRequest, search: Search, fields: dict, codes: dict, facets: list):
-    return solr_response
+    new_sort = f'format asc, {solr_query["sort"]}'
+    solr_query['sort'] = new_sort
+    return solr_query
 
 
 def pre_mlt_solr_query(context: dict, solr_query: dict, request: HttpRequest, search: Search, fields: dict, codes: dict, record_is: str):
@@ -48,4 +50,7 @@ def load_csv_record(csv_record: dict, solr_record: dict, search: Search, fields:
         solr_record['title_en'] = str(solr_record['title_en']).strip()
     if solr_record['title_fr']:
         solr_record['title_fr'] = str(solr_record['title_fr']).strip()
+    if format == 'NTR':
+        id_str = f"{solr_record['owner_org']},{solr_record['year']},{solr_record['month']}"
+        solr_record['id'] = id_str
     return solr_record
--- qpnotes/plugins/qpnotes.py
+++ qpnotes/plugins/qpnotes.py
@@ -26,6 +26,8 @@ def post_record_solr_query(context: dict, solr_response: SolrResponse, solr_quer
 
 
 def pre_export_solr_query(solr_query: dict, request: HttpRequest, search: Search, fields: dict, codes: dict, facets: list):
+    new_sort = f'format asc, {solr_query["sort"]}'
+    solr_query['sort'] = new_sort
     return solr_query
 
 
@@ -47,38 +49,42 @@ def filter_csv_record(csv_record,search: Search, fields: dict, codes: dict, form
 
 def load_csv_record(csv_record: dict, solr_record: dict, search: Search, fields: dict, codes: dict, format: str):
     try:
-        local_tz = "US/Eastern"
-        received_date = datetime.strptime(csv_record['date_received'], '%Y-%m-%d').replace(tzinfo=pytz.timezone(local_tz))
-        field = Field.objects.get(field_id='minister', search_id=search.search_id)
+        if format != 'NTR':
+            local_tz = "US/Eastern"
+            received_date = datetime.strptime(csv_record['date_received'], '%Y-%m-%d').replace(tzinfo=pytz.timezone(local_tz))
+            field = Field.objects.get(field_id='minister', search_id=search.search_id)
 
-        if csv_record['minister']:
-            code = Code.objects.get(field_fid=field, code_id=csv_record['minister'])
-            ccodes = ChronologicCode.objects.filter(code_cid=code).filter(start_date__lte=received_date).filter(end_date__gte=received_date)
-            if len(ccodes) > 0:
-                ccode: ChronologicCode = ccodes[0]
-                solr_record['minister_name_en'] = ccode.label_en
-                solr_record['minister_name_fr'] = ccode.label_fr
-            else:
-                # Try matching by year instead
-                received_date = datetime.strptime(csv_record['date_received'], '%Y-%m-%d').replace(month=12, day=31,
-                    tzinfo=pytz.timezone(local_tz))
-                ccodes = ChronologicCode.objects.filter(code_cid=code).filter(
-                    start_date__lte=received_date).filter(end_date__gte=received_date)
+            if csv_record['minister']:
+                code = Code.objects.get(field_fid=field, code_id=csv_record['minister'])
+                ccodes = ChronologicCode.objects.filter(code_cid=code).filter(start_date__lte=received_date).filter(end_date__gte=received_date)
                 if len(ccodes) > 0:
                     ccode: ChronologicCode = ccodes[0]
                     solr_record['minister_name_en'] = ccode.label_en
                     solr_record['minister_name_fr'] = ccode.label_fr
                 else:
-                # Just take the last minister then
-                    ccodes = ChronologicCode.objects.filter(code_cid=code)
+                    # Try matching by year instead
+                    received_date = datetime.strptime(csv_record['date_received'], '%Y-%m-%d').replace(month=12, day=31,
+                        tzinfo=pytz.timezone(local_tz))
+                    ccodes = ChronologicCode.objects.filter(code_cid=code).filter(
+                        start_date__lte=received_date).filter(end_date__gte=received_date)
                     if len(ccodes) > 0:
                         ccode: ChronologicCode = ccodes[0]
                         solr_record['minister_name_en'] = ccode.label_en
                         solr_record['minister_name_fr'] = ccode.label_fr
-        if solr_record['title_en']:
-            solr_record['title_en'] = str(solr_record['title_en']).strip()
-        if solr_record['title_fr']:
-            solr_record['title_fr'] = str(solr_record['title_fr']).strip()
+                    else:
+                    # Just take the last minister then
+                        ccodes = ChronologicCode.objects.filter(code_cid=code)
+                        if len(ccodes) > 0:
+                            ccode: ChronologicCode = ccodes[0]
+                            solr_record['minister_name_en'] = ccode.label_en
+                            solr_record['minister_name_fr'] = ccode.label_fr
+            if solr_record['title_en']:
+                solr_record['title_en'] = str(solr_record['title_en']).strip()
+            if solr_record['title_fr']:
+                solr_record['title_fr'] = str(solr_record['title_fr']).strip()
+        else:
+            id_str = f'{solr_record["owner_org"]},{solr_record["year"]},{solr_record["reporting_period"]}'
+            solr_record['id'] = id_str
     except Exception as e:
         print(e)
     return solr_record
